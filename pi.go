7b8b4efabb4f6d9f706e89a11b5711985490fbce
Shugo Kawamura <shugo_k@flowing.tokyo>
2022-12-12 05:03:19 +0900

    Fix typing

--- pi.go
+++ pi.go
@@ -1,15 +1,21 @@
 package streamdeck
 
+// Settings settings for PI
+type Settings interface {
+	Initalize()
+	IsDefault() bool
+}
+
 // PropertyInspector PI settings
-type PropertyInspector[T any] struct {
+type PropertyInspector[T Settings] struct {
 	Settings T
 }
 
 // PI をactionに紐付けてKeyDownHandlerの際に活用出来るようにする
 
 // NewPropertyInspector Get new PI
-func NewPropertyInspector[T any](settings T) PropertyInspector[any] {
-	return PropertyInspector[any]{
+func NewPropertyInspector[T Settings](settings T) PropertyInspector[T] {
+	return PropertyInspector[T]{
 		Settings: settings,
 	}
 }
