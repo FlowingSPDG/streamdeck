f338a9dd1a67fdc08e82ad1e2cced3a19b2ffd2d
Shugo Kawamura <shugo_k@flowing.tokyo>
2022-12-19 07:48:20 +0900

    接続処理のリファクタリング

--- wasm/wasm.go
+++ wasm/wasm.go
@@ -14,21 +14,29 @@ import (
 	"nhooyr.io/websocket"
 )
 
-func DeclarePropertyInspectorRegistration[S any]() {
+func DeclarePropertyInspectorRegistration[S any]() error {
 	js.Global().Set("std_connected", false)
-
-	// これならパースしてString()とかでも良いのでは？
-	vals := make([]js.Value, 0, 5)
-	vals = append(vals, js.Global().Get("port"))
-	vals = append(vals, js.Global().Get("uuid"))
-	vals = append(vals, js.Global().Get("registerEventName"))
-	vals = append(vals, js.Global().Get("Info"))
-	vals = append(vals, js.Global().Get("actionInfo"))
 
 	// wasmを読み込む前にconnectElgatoStreamDeckSocketが走ってしまうので、
 	// wasmロード前に受け取った値をグローバルに保存して、ロードが終わり次第wasm側から起動する
-	js.Global().Set("connectElgatoStreamDeckSocketJS", js.FuncOf(connectElgatoStreamDeckSocketJS[S]))
-	connectElgatoStreamDeckSocketJS[S](js.Undefined(), vals)
+	inPort := js.Global().Get("port").Int()
+	inPropertyInspectorUUID := js.Global().Get("uuid").String()
+	inRegisterEvent := js.Global().Get("registerEventName").String() // should be "registerPropertyInspector"
+	inInfo := inInfo{}
+	if err := json.Unmarshal([]byte(js.Global().Get("Info").String()), &inInfo); err != nil {
+		fmt.Println("Failed to parse inInfo:", err)
+		return err
+	}
+	inActionInfo := inActionInfo[S]{}
+	if err := json.Unmarshal([]byte(js.Global().Get("actionInfo").String()), &inActionInfo); err != nil {
+		fmt.Println("Failed to parse inInfo:", err)
+		return err
+	}
+	if err := connectElgatoStreamDeckSocket[S](inPort, inPropertyInspectorUUID, inRegisterEvent, inInfo, inActionInfo); err != nil {
+		fmt.Println("Failed to connect ElgatoStreamDeckSocket:", err)
+		return err
+	}
+	return nil
 }
 
 // DidReceiveSettings を受信したり、WebSocketの接続が確立した時にJSに変数を格納したい
@@ -36,28 +44,7 @@ func DeclarePropertyInspectorRegistration[S any]() {
 // function connectElgatoStreamDeckSocket(inPort, inPropertyInspectorUUID, inRegisterEvent, inInfo, inActionInfo)
 // e.g.
 // connectElgatoStreamDeckSocket(28196, "F25D3773EA4693AB3C1B4323EA6B00D1", "registerPropertyInspector", '{"application":{"font":".AppleSystemUIFont","language":"en","platform":"mac","platformVersion":"13.1.0","version":"6.0.1.17722"},"colors":{"buttonPressedBackgroundColor":"#303030FF","buttonPressedBorderColor":"#646464FF","buttonPressedTextColor":"#969696FF","disabledColor":"#007AFF7F","highlightColor":"#007AFFFF","mouseDownColor":"#2EA8FFFF"},"devicePixelRatio":2,"devices":[{"id":"7EAEBEB876DC1927A04E7E31610731CF","name":"Stream Deck","size":{"columns":5,"rows":3},"type":0}],"plugin":{"uuid":"dev.flowingspdg.newtek","version":"0.1.4"}}', '{"action":"dev.flowingspdg.newtek.shortcuttcp","context":"52ba9e6590bf53c7ff96b89d61c880b7","device":"7EAEBEB876DC1927A04E7E31610731CF","payload":{"controller":"Keypad","coordinates":{"column":3,"row":2},"settings":{"host":"192.168.100.93","shortcut":"mode","value":"2"}}}')
-func connectElgatoStreamDeckSocketJS[SettingsT any](this js.Value, args []js.Value) any {
-	fmt.Println("connectElgatoStreamDeckSocket")
-	inPort := args[0].Int()
-	inPropertyInspectorUUID := args[1].String()
-	inRegisterEvent := args[2].String() // should be "registerPropertyInspector"
-	inInfo := inInfo{}
-	if err := json.Unmarshal([]byte(args[3].String()), &inInfo); err != nil {
-		fmt.Println("Failed to parse inInfo:", err)
-		return err
-	}
-	inActionInfo := inActionInfo[SettingsT]{}
-	if err := json.Unmarshal([]byte(args[4].String()), &inActionInfo); err != nil {
-		fmt.Println("Failed to parse inInfo:", err)
-		return err
-	}
-
-	connectElgatoStreamDeckSocket(inPort, inPropertyInspectorUUID, inRegisterEvent, inInfo, inActionInfo)
-
-	return nil
-}
-
-func connectElgatoStreamDeckSocket[SettingsT any](inPort int, inPropertyInspectorUUID string, inRegisterEvent string, inInfo inInfo, inActionInfo inActionInfo[SettingsT]) {
+func connectElgatoStreamDeckSocket[SettingsT any](inPort int, inPropertyInspectorUUID string, inRegisterEvent string, inInfo inInfo, inActionInfo inActionInfo[SettingsT]) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
@@ -77,7 +64,7 @@ func connectElgatoStreamDeckSocket[SettingsT any](inPort int, inPropertyInspecto
 	if err != nil {
 		// TODO: handle error
 		fmt.Println("Failed to connect websocket:", err.Error())
-		return
+		return err
 	}
 	// TODO: defer to close websocket
 
@@ -95,8 +82,11 @@ func connectElgatoStreamDeckSocket[SettingsT any](inPort int, inPropertyInspecto
 	if err := sdc.Register(ctx); err != nil {
 		// TODO: handle error
 		fmt.Println("Failed to register Property Inspector:", err.Error())
-		return
+		return err
 	}
+
+	// window.$SD に設定するとJavaScriptからも利用が可能になる
 	wrapper.RegisterGlobal("$SD")
 	js.Global().Set("std_connected", true)
+	return nil
 }
